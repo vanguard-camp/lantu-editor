6a104088ae69fdd04c1224b818d1a24bbdd29eaf
Mengsio <ssmdmsn@hotmail.com>
2024-02-03 12:59:19 +0800

    feat: turbo project

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,28 +8,7 @@ module.exports = {
     'standard',
     'standard-jsx'
   ],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    '@typescript-eslint',
-    'react'
-  ],
   rules: {
     'no-unused-vars': 'off',
     'no-useless-constructor': 'off'
